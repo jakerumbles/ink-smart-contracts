36afb7a633b96fd61ffcce2bcbc8e84d93405791
Jake Edwards <edwardsjake@live.com>
2022-04-22 14:07:28 -0500

    Working through ERC20 tutorial

---
+++ erc20/lib.rs
@@ -0,0 +1,128 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+#[ink::contract]
+mod erc20 {
+    use ink_storage::traits::SpreadAllocate;
+
+    #[cfg(not(feature = "ink-as-dependency"))]
+    #[ink(storage)]
+    #[derive(SpreadAllocate)]
+    pub struct Erc20 {
+        /// The total supply.
+        total_supply: Balance,
+        /// The balance of each user.
+        balances: ink_storage::Mapping<AccountId, Balance>,
+    }
+
+    /// Specify ERC-20 error type
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        /// Return if the  balance cannot fulfill a request
+        InsufficientBalance,
+    }
+
+    pub type Result<T> = core::result::Result<T, Error>;
+
+    impl Erc20 {
+        #[ink(constructor)]
+        pub fn new(initial_supply: Balance) -> Self {
+            // ACTION: `set` the total supply to `initial_supply`
+            // ACTION: `insert` the `initial_supply` as the `caller` balance
+            ink_lang::utils::initialize_contract(|contract: &mut Self| {
+                contract.total_supply = initial_supply;
+                let caller = contract.env().caller();
+                contract.balances.insert(&caller, &initial_supply);
+            })
+        }
+
+        #[ink(message)]
+        pub fn total_supply(&self) -> Balance {
+            // ACTION: Return the total supply
+            self.total_supply
+        }
+
+        #[ink(message)]
+        pub fn balance_of(&self, owner: AccountId) -> Balance {
+            // ACTION: Return the balance of `owner`
+            //   HINT: Use `balance_of_or_zero` to get the `owner` balance
+            self.balance_of_or_zero(&owner)
+        }
+
+        fn balance_of_or_zero(&self, owner: &AccountId) -> Balance {
+            // ACTION: `get` the balance of `owner`, then `unwrap_or` fallback to 0
+            self.balances.get(owner).unwrap_or(0)
+        }
+
+        #[ink(message)]
+        pub fn transfer(&mut self, to: AccountId, value: Balance) -> Result<()> {
+            let from = self.env().caller();
+            self.transfer_from_to(&from, &to, value)
+        }
+
+        fn transfer_from_to(
+            &mut self,
+            from: &AccountId,
+            to: &AccountId,
+            value: Balance,
+        ) -> Result<()> {
+            let from_balance = self.balance_of_impl(from);
+            if from_balance < value {
+                return Err(Error::InsufficientBalance);
+            }
+
+            // `from` account has enough to send transfer
+            self.balances.insert(from, &(from_balance - value));
+            let to_balance = self.balance_of_impl(to);
+            self.balances.insert(to, &(to_balance + value));
+            Ok(())
+        }
+
+        #[inline]
+        fn balance_of_impl(&self, owner: &AccountId) -> Balance {
+            self.balances.get(owner).unwrap_or_default()
+        }
+
+        #[ink(message)]
+        pub fn get_caller_address(&self) -> AccountId {
+            self.env().caller()
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        use ink_lang as ink;
+
+        #[ink::test]
+        fn new_works() {
+            let contract = Erc20::new(777);
+            assert_eq!(contract.total_supply(), 777);
+        }
+
+        #[ink::test]
+        fn balance_works() {
+            let contract = Erc20::new(100);
+            assert_eq!(contract.total_supply(), 100);
+            assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
+            assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 0);
+        }
+
+        #[ink::test]
+        fn transfer_works() {
+            let mut contract = Erc20::new(100);
+
+            let alice = AccountId::from([0x1; 32]);
+            let bob = AccountId::from([0x0; 32]);
+
+            // transfer 25 from alice to bob
+            let _result = contract.transfer(bob, 25);
+
+            assert_eq!(contract.balance_of(alice), 75);
+            assert_eq!(contract.balance_of(bob), 25);
+        }
+    }
+}
